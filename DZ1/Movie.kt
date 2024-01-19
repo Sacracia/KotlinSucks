987ade2f302b9dcefc4321dc5ef970f606d56054
Sacracia <tiger3551999@mail.ru>
2024-01-19 23:10:27 +0300

    19.1.2024

--- DZ1/Movie.kt
+++
@@ -1,5 +0,0 @@
-class Movie(var title: String) {
-    override fun toString(): String {
-        return title
-    }
-}
--- DZ1/User.kt
+++
@@ -1,6 +0,0 @@
-class User(val name:String, val surname: String) {
-
-    override fun toString(): String {
-        return "$surname $name"
-    }
-}
